773432cbadb47944e4cbd07472c9f0c5040e6fdf
Felipe Ribeiro Tanso <felipetanso@gmail.com>
2022-05-24 18:51:26 -0300

    Adicao de paginas fullscreen

--- public/scripts.js
+++ public/scripts.js
@@ -34,12 +34,14 @@ function TemaHachi(){
 //   GRIDS //
 
 function DataDriven_1(){
-    window.location.href = 'fullScreen.html';
+    window.location.href = 'datadriven_1.html';
 };
 
 function DataDriven_2(){
-    window.location.href = 'fullScreen.html';
+    window.location.href = 'datadriven_2.html';
 };
+
+ // educacao //
 
 function Educacao_1(){
     window.location.href = 'fullScreen.html';
@@ -57,13 +59,115 @@ function Educacao_4(){
     window.location.href = 'fullScreen.html';
 };
 
-function GridIchi(){
+// mulheres //
+
+function Mulheres_1(){
     window.location.href = 'fullScreen.html';
 };
 
-function GridIchi(){
+function Mulheres_2(){
     window.location.href = 'fullScreen.html';
 };
+
+function Mulheres_3(){
+    window.location.href = 'fullScreen.html';
+};
+
+function Mulheres_4(){
+    window.location.href = 'fullScreen.html';
+};
+
+// saude //
+
+function Saude_1(){
+    window.location.href = 'fullScreen.html';
+};
+
+function Saude_2(){
+    window.location.href = 'fullScreen.html';
+};
+
+function Saude_3(){
+    window.location.href = 'fullScreen.html';
+};
+
+function Saude_4(){
+    window.location.href = 'fullScreen.html';
+};
+
+// tecnologia //
+
+function Tecnologia_1(){
+    window.location.href = 'fullScreen.html';
+};
+
+function Tecnologia_2(){
+    window.location.href = 'fullScreen.html';
+};
+
+function Tecnologia_3(){
+    window.location.href = 'fullScreen.html';
+};
+
+function Tecnologia_4(){
+    window.location.href = 'fullScreen.html';
+};
+
+// trabalho //
+
+function Trabalho_1(){
+    window.location.href = 'fullScreen.html';
+};
+
+function Trabalho_2(){
+    window.location.href = 'fullScreen.html';
+};
+
+function Trabalho_3(){
+    window.location.href = 'fullScreen.html';
+};
+
+function Trabalho_4(){
+    window.location.href = 'fullScreen.html';
+};
+
+// macrossetoria //
+
+function Macrossetorial_1(){
+    window.location.href = 'fullScreen.html';
+};
+
+function Macrossetorial_2(){
+    window.location.href = 'fullScreen.html';
+};
+
+function Macrossetorial_3(){
+    window.location.href = 'fullScreen.html';
+};
+
+function Macrossetorial_4(){
+    window.location.href = 'fullScreen.html';
+};
+
+// industria //
+
+function Industria_1(){
+    window.location.href = 'fullScreen.html';
+};
+
+function Industria_2(){
+    window.location.href = 'fullScreen.html';
+};
+
+function Industria_3(){
+    window.location.href = 'fullScreen.html';
+};
+
+function Industria_4(){
+    window.location.href = 'fullScreen.html';
+};
+
+
 
 //voltar function
 function Voltar(){
