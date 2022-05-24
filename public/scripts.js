d9594c61e966b56511de7bfe6a7e4fc5b0e71c01
Felipe Ribeiro Tanso <felipetanso@gmail.com>
2022-05-24 19:30:35 -0300

    Atualizacao do Scripts e Grid Educacao

--- public/scripts.js
+++ public/scripts.js
@@ -44,127 +44,127 @@ function DataDriven_2(){
  // educacao //
 
 function Educacao_1(){
-    window.location.href = 'fullScreen.html';
+    window.location.href = 'educacao_1.html';
 };
 
 function Educacao_2(){
-    window.location.href = 'fullScreen.html';
+    window.location.href = 'educacao_2.html';
 };
 
 function Educacao_3(){
-    window.location.href = 'fullScreen.html';
+    window.location.href = 'educacao_3.html';
 };
 
 function Educacao_4(){
-    window.location.href = 'fullScreen.html';
+    window.location.href = 'educacao_4.html';
 };
 
 // mulheres //
 
 function Mulheres_1(){
-    window.location.href = 'fullScreen.html';
+    window.location.href = 'mulheres_1.html';
 };
 
 function Mulheres_2(){
-    window.location.href = 'fullScreen.html';
+    window.location.href = 'mulheres_2.html';
 };
 
 function Mulheres_3(){
-    window.location.href = 'fullScreen.html';
+    window.location.href = 'mulheres_3.html';
 };
 
 function Mulheres_4(){
-    window.location.href = 'fullScreen.html';
+    window.location.href = 'mulheres_4.html';
 };
 
 // saude //
 
 function Saude_1(){
-    window.location.href = 'fullScreen.html';
+    window.location.href = 'saude_1.html';
 };
 
 function Saude_2(){
-    window.location.href = 'fullScreen.html';
+    window.location.href = 'saude_2.html';
 };
 
 function Saude_3(){
-    window.location.href = 'fullScreen.html';
+    window.location.href = 'saude_3.html';
 };
 
 function Saude_4(){
-    window.location.href = 'fullScreen.html';
+    window.location.href = 'saude_4.html';
 };
 
 // tecnologia //
 
 function Tecnologia_1(){
-    window.location.href = 'fullScreen.html';
+    window.location.href = 'tecnologia_1.html';
 };
 
 function Tecnologia_2(){
-    window.location.href = 'fullScreen.html';
+    window.location.href = 'tecnologia_2.html';
 };
 
 function Tecnologia_3(){
-    window.location.href = 'fullScreen.html';
+    window.location.href = 'tecnologia_3.html';
 };
 
 function Tecnologia_4(){
-    window.location.href = 'fullScreen.html';
+    window.location.href = 'tecnologia_4.html';
 };
 
 // trabalho //
 
 function Trabalho_1(){
-    window.location.href = 'fullScreen.html';
+    window.location.href = 'trabalho_1.html';
 };
 
 function Trabalho_2(){
-    window.location.href = 'fullScreen.html';
+    window.location.href = 'trabalho_2.html';
 };
 
 function Trabalho_3(){
-    window.location.href = 'fullScreen.html';
+    window.location.href = 'trabalho_3.html';
 };
 
 function Trabalho_4(){
-    window.location.href = 'fullScreen.html';
+    window.location.href = 'trabalho_4.html';
 };
 
 // macrossetoria //
 
 function Macrossetorial_1(){
-    window.location.href = 'fullScreen.html';
+    window.location.href = 'macrossetorial_1.html';
 };
 
 function Macrossetorial_2(){
-    window.location.href = 'fullScreen.html';
+    window.location.href = 'macrossetorial_2.html';
 };
 
 function Macrossetorial_3(){
-    window.location.href = 'fullScreen.html';
+    window.location.href = 'macrossetorial_3.html';
 };
 
 function Macrossetorial_4(){
-    window.location.href = 'fullScreen.html';
+    window.location.href = 'macrossetorial_4.html';
 };
 
 // industria //
 
 function Industria_1(){
-    window.location.href = 'fullScreen.html';
+    window.location.href = 'industria_1.html';
 };
 
 function Industria_2(){
-    window.location.href = 'fullScreen.html';
+    window.location.href = 'industria_2.html';
 };
 
 function Industria_3(){
-    window.location.href = 'fullScreen.html';
+    window.location.href = 'industria_3.html';
 };
 
 function Industria_4(){
-    window.location.href = 'fullScreen.html';
+    window.location.href = 'industria_4.html';
 };
 
 
