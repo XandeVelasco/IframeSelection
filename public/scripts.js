6da801214dfc70a526bba8de23ac92b4371f7641
Alexandre Velasco Alves <54082209+XandeVelasco@users.noreply.github.com>
2022-05-24 11:20:14 -0300

    Adição de graficos
    Adicionado mais graficos ao menu DataDriven

---
+++ public/scripts.js
@@ -0,0 +1,43 @@
+function TemaIchi(){
+    window.location.href = 'gridDataDriven.html';
+};
+
+function TemaNi(){
+    alert("Ni was clicked.");
+};
+
+function TemaSan(){
+    alert("San was clicked.");
+};
+
+function TemaYon(){
+    alert("Yon was clicked.");
+};
+
+function TemaGo(){
+    alert("Go was clicked.");
+};
+
+function TemaRoku(){
+    alert("Roku was clicked.");
+};
+
+function TemaNana(){
+    alert("Nana was clicked.");
+};
+
+function TemaHachi(){
+    alert("Hachi was clicked.");
+};
+
+
+//   GRIDS //
+
+function GridIchi(){
+    window.location.href = 'fullScreen.html';
+};
+
+//voltar function
+function Voltar(){
+    
+};
